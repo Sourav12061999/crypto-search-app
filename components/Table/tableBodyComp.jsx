69c7e77703a6daa0a33661916a70c5743b53b0da
Sourav Das <sdunity12061999@gmail.com>
2022-03-12 11:42:17 +0530

    table component ready

---
+++ components/Table/tableBodyComp.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+function TableBodyComp({ row }) {
+  return (
+    <TableRow
+      key={row.name}
+      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+    >
+      <TableCell component="th" scope="row">
+        {row.name}
+      </TableCell>
+      <TableCell align="right">{row.calories}</TableCell>
+      <TableCell align="right">{row.fat}</TableCell>
+      <TableCell align="right">{row.carbs}</TableCell>
+      <TableCell align="right">{row.protein}</TableCell>
+    </TableRow>
+  );
+}
+
+export default TableBodyComp;
--- components/Table/tableComp.jsx
+++ components/Table/tableComp.jsx
@@ -4,9 +4,10 @@ import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-
+import TableRow from "@mui/material/TableRow";
+import { Typography } from "@mui/material";
+import TableBodyComp from "./tableBodyComp";
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
@@ -25,27 +26,18 @@ export default function TableComp() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Coin Name</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Marget Cap</TableCell>
+            <TableCell align="right">24H High</TableCell>
+            <TableCell align="right">24H Low</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
+            <React.Fragment key={row.name}>
+              <TableBodyComp row={row} />
+            </React.Fragment>
           ))}
         </TableBody>
       </Table>
