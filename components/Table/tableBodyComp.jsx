8506d466a396f0bd5fe297aead476ea63c1ebfd9
Sourav Das <sdunity12061999@gmail.com>
2022-03-13 11:53:08 +0530

    coin details page added

--- components/Table/tableBodyComp.jsx
+++ components/Table/tableBodyComp.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 import { Box } from "@mui/system";
+import { useRouter } from "next/router";
 function TableBodyComp({ row, index }) {
+  const router = useRouter();
   return (
     <TableRow
       sx={{
         "&:last-child td, &:last-child th": { border: 0 },
         backgroundColor: index % 2 !== 0 ? "rgb(240, 240, 240)" : "white",
+        ":hover": {
+          cursor: "pointer",
+        },
+      }}
+      onClick={() => {
+        router.replace(`/currency/${row.id}`);
       }}
     >
       <TableCell
